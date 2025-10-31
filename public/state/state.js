a46c61243a0302e88ea88ac2170fe6f2241fefc3
Amir <vibelessyoung@gmail.com>
2025-10-31 18:19:00 +0330

    add todo rendering

--- public/state/state.js
+++ public/state/state.js
@@ -4,8 +4,16 @@ export const state = {
 
 export function addTodo(todoText) {
   state.TodosState.push(todoText);
+  localStorage.setItem("todos", JSON.stringify(state.TodosState));
 }
 
 export function getTodos() {
   return state.TodosState;
+}
+
+export function loadTodos() {
+  const saved = localStorage.getItem("todos");
+  if (saved) {
+    state.TodosState = JSON.parse(saved);
+  }
 }
